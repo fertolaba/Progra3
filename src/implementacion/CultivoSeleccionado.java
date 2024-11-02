4836718554d77b4e3fb8f54abd9f3014c95902f4
Melina <melurod2003@gmail.com>
2024-11-02 12:57:30 -0300

    update

---
+++ src/implementacion/CultivoSeleccionado.java
@@ -0,0 +1,27 @@
+package implementacion;
+
+public class CultivoSeleccionado {
+
+    private String nombreCultivo;
+    private double montoInvertido;
+    private double riesgoPromedio;
+    private double gananciaObtenida;
+
+    //falta agregar las coordenadas de la esquina superior izq y la inf derecha del area
+
+    public String getNombreCultivo() {
+        return nombreCultivo;
+    }
+
+    public double getMontoInvertido() {
+        return montoInvertido;
+    }
+
+    public double getRiesgoPromedio() {
+        return riesgoPromedio;
+    }
+
+    public double getGananciaObtenida() {
+        return gananciaObtenida;
+    }
+}
--- src/implementacion/PlanificarCultivos.java
+++ src/implementacion/PlanificarCultivos.java
@@ -16,8 +16,37 @@ public class PlanificarCultivos implements Lib.PlanificarCultivos {
         return mejorDistribucion;
     }
 
-    private void backtracking(double[][] campo, List<Cultivo> cultivos, String temporadaActual, int x, int y,
+    private List<CultivoSeleccionado> backtracking(double[][] campo, List<Cultivo> cultivos, String temporadaActual, int x, int y,
                               List<CultivoSeleccionado> distribucionActual, List<CultivoSeleccionado> mejorDistribucion){
+        if (x>=campo.length){
+            double gananciaActual= sumaGanancias(distribucionActual);
+            double gananciaMejor=sumaGanancias(mejorDistribucion);
+
+            if (gananciaActual>gananciaMejor){
+                mejorDistribucion=distribucionActual;
+            }
+            return mejorDistribucion;
+        }
+
+        for (int i=0; i<cultivos.size(); i++){
+            Cultivo cultivo=cultivos.get(i);
+            if(puedoUbicar(campo, cultivo, x, y, temporadaActual)){
+                double ganancia=gananciaCultivo(cultivo,campo, x, y);
+                double riesgoPromedio=riesgoPromedio(cultivo, campo, x, y);
+
+                distribucionActual.add();
+
+                int sig_x=x;
+                int sig_y= y + cultivo.getColumnas();
+                if(sig_y>campo[0].length){
+                    sig_x+=1;
+                    sig_y=0;
+                }
+
+                backtracking(campo, cultivos, temporadaActual, x, y, distribucionActual, mejorDistribucion);
+                //eliminar(distrib_Actual)
+            }
+        }
 
     }
 
@@ -62,5 +91,13 @@ public class PlanificarCultivos implements Lib.PlanificarCultivos {
         }
         double riesgoPromedio=riesgoTotal/(cultivo.getFilas()*cultivo.getColumnas());
         return riesgoPromedio;
+    }
+
+    private double sumaGanancias(List<CultivoSeleccionado> distribucion){
+        double total=0;
+        for(int i=0; i< distribucion.size();i++){
+            total+=distribucion.get(i).getGananciaObtenida();
+        }
+        return total;
     }
 }
